42c70585f300b9fb0d4af93a3bfb16551c5e6a1e
KorolevaAlena <gorbunovalenka@yandex.ru>
2024-07-23 11:42:15 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework = 12
+time = 1.5
+name = 'Python'
+time_for_one_task = time/homework
+print ("Курс:"+str(name)+", всего задач:"+str(homework)+", затрачено часов:"+str(time)+", среднее время выполнения ",time_for_one_task,"часа.")
